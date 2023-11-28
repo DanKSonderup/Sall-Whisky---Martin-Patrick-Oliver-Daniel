6584c9196e5ab349a3cfcefd303d79f68fefbcd5
PatrickTyme <129082542+PatrickTyme@users.noreply.github.com>
2023-11-28 10:41:56 +0100

    Getters & setters casksupplier

--- Sall Whisky/src/model/CaskSupplier.java
+++ Sall Whisky/src/model/CaskSupplier.java
@@ -12,4 +12,38 @@ public class CaskSupplier {
         this.country = country;
         this.vatId = vatId;
     }
+
+    /** Getters and setters */
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getVatId() {
+        return vatId;
+    }
+
+    public void setVatId(String vatId) {
+        this.vatId = vatId;
+    }
 }
