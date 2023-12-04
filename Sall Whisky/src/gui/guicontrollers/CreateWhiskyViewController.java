422fec0f819e9f9a7b92c51d7911600f2af2fc57
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2023-12-04 13:47:25 +0100

    Gui updates

---
+++ Sall Whisky/src/gui/guicontrollers/CreateWhiskyViewController.java
@@ -0,0 +1,82 @@
+package gui.guicontrollers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class CreateWhiskyViewController {
+    @FXML
+    private Label FillOnWhiskylbl;
+
+    @FXML
+    private ListView<?> FillOnWhiskylvw;
+
+    @FXML
+    private ListView<?> WhiskyPropertieslvw;
+
+    @FXML
+    private Label amountOfBottleslbl;
+
+    @FXML
+    private TextField amountOfBottlestxf;
+
+    @FXML
+    private Label amountOfFillCllbl;
+
+    @FXML
+    private TextField amountOfFillCltxf;
+
+    @FXML
+    private ComboBox<?> bottleSizecbb;
+
+    @FXML
+    private Label bottleSizelbl;
+
+    @FXML
+    private Button btnCreateWhisky;
+
+    @FXML
+    private Button btnRegistrerAlcoholPercentage;
+
+    @FXML
+    private TextField txfAlcoholPercentage;
+
+    @FXML
+    private TextField txfTypeOfWhisky;
+
+    @FXML
+    private Label typeOfWhiskylbl;
+
+    @FXML
+    private ListView<?> typeOfWhiskylvw;
+
+    @FXML
+    private Label wateredWhiskylbl;
+
+    @FXML
+    private TextField wateredWhiskytxf;
+
+    @FXML
+    private Label whiskyPropertieslbl;
+
+    @FXML
+    void amountOfBottlesCalculation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCreateWhisky(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegisterAlcoholPercentageOnAction(ActionEvent event) {
+
+    }
+
+}
+
