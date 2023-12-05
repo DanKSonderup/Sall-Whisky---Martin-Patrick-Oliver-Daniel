10c37df5893d951f63fc47962bb3d35ac94482fd
PatrickTyme <129082542+PatrickTyme@users.noreply.github.com>
2023-12-05 10:48:44 +0100

    CRUDWarehouseView createbutton funktionalitet lavet

--- Sall Whisky/src/gui/guicontrollers/CRUDWarehouseViewController.java
+++ Sall Whisky/src/gui/guicontrollers/CRUDWarehouseViewController.java
@@ -1,22 +1,20 @@
 package gui.guicontrollers;
 
         import controller.MainController;
+        import javafx.beans.value.ChangeListener;
         import javafx.event.ActionEvent;
         import javafx.fxml.FXML;
+        import javafx.fxml.Initializable;
         import javafx.scene.control.Button;
         import javafx.scene.control.Label;
         import javafx.scene.control.ListView;
         import javafx.scene.control.TextField;
         import javafx.scene.layout.AnchorPane;
-        import model.Position;
-        import model.Rack;
-        import model.Shelf;
-        import model.Warehouse;
-
+        import model.*;
         import java.net.URL;
         import java.util.ResourceBundle;
 
-public class CRUDWarehouseViewController {
+public class CRUDWarehouseViewController implements Initializable {
 
     @FXML
     private AnchorPane ap_Pane1;
@@ -64,30 +62,16 @@ public class CRUDWarehouseViewController {
     private TextField rackAmounttxf;
 
     @FXML
+    private Label warehouseAddresslbl;
+
+    @FXML
+    private TextField warehouseAddresstxf;
+
+    @FXML
     private Label warehouselbl;
-
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        lvwWarehouse.getItems().setAll(MainController.getStorage().getWarehouses());
-        for (Warehouse warehouse : MainController.getStorage().getWarehouses()) {
-           lvwRack.getItems().setAll(warehouse.getRacks());
-        }
-
-        }
-
-    @FXML
-    void btnCreateWarehouseOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnCrudWarehouseOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnDeleteWarehouseOnAction(ActionEvent event) {
-
-    }
+    private Rack rack;
+    private Shelf shelf;
+    private Position position;
 
     @FXML
     void btnDestillateAndFillOnCaskOnAction(ActionEvent event) {
@@ -106,7 +90,61 @@ public class CRUDWarehouseViewController {
 
     @FXML
     void btnSupplierOnAction(ActionEvent event) {
+    }
 
+    private Warehouse currentlySelectedWarehouse;
+    private Rack currentlySelectedRack;
+    private Shelf currentlySelectedShelf;
+    private Warehouse warehouse;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ChangeListener<Warehouse> warehouseChangeListener = (ov, o, n) -> this.selectedStorageItemChanged();
+        lvwWarehouse.getSelectionModel().selectedItemProperty().addListener(warehouseChangeListener);
+
+        ChangeListener<Rack> rackChangeListener = (ov, o, n) -> this.selectedStorageItemChanged();
+        lvwRack.getSelectionModel().selectedItemProperty().addListener(rackChangeListener);
+
+        ChangeListener<Shelf> shelfChangeListener = (ov, o, n) -> this.selectedStorageItemChanged();
+        lvwShelf.getSelectionModel().selectedItemProperty().addListener(shelfChangeListener);
+
+        ChangeListener<Position> positionChangeListener = (ov, o, n) -> this.selectedStorageItemChanged();
+        lvwPosition.getSelectionModel().selectedItemProperty().addListener(positionChangeListener);
+
+        lvwWarehouse.getItems().setAll(MainController.getStorage().getWarehouses());
+
+        }
+
+    @FXML
+    void btnCreateWarehouseOnAction(ActionEvent event) {
+        boolean missingInfo = canParseToDouble(rackAmounttxf);
+
+        if (!missingInfo) {
+            System.out.println("hello");
+            warehouse = MainController.createWarehouse(warehouseAddresstxf.getText());
+            for (int i = 0; i < Integer.parseInt(rackAmounttxf.getText()); i++) {
+                rack = MainController.createRack(warehouse);
+                for (int j = 0; j < 5; j++) {
+                    shelf = MainController.createShelf(rack);
+                    for (int k = 0; k < 5; k++) {
+                        position = MainController.createPosition(shelf, 500);
+                    }
+                }
+            }
+        }
+        updateWarehouses();
+        rackAmounttxf.clear();
+        warehouseAddresstxf.clear();
+
+    }
+
+    @FXML
+    void btnCrudWarehouseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteWarehouseOnAction(ActionEvent event) {
+        // TODO
     }
 
     @FXML
@@ -114,4 +152,50 @@ public class CRUDWarehouseViewController {
 
     }
 
+    public void selectedStorageItemChanged() {
+        Warehouse selectedWarehouse = lvwWarehouse.getSelectionModel().getSelectedItem();
+        Rack selectedRack = lvwRack.getSelectionModel().getSelectedItem();
+        Shelf selectedShelf = lvwShelf.getSelectionModel().getSelectedItem();
+
+        if (selectedWarehouse == null) return;
+        if (selectedWarehouse != currentlySelectedWarehouse) {
+            currentlySelectedWarehouse = selectedWarehouse;
+            lvwRack.getItems().setAll(currentlySelectedWarehouse.getRacks());
+            lvwShelf.getItems().clear();
+            lvwPosition.getItems().clear();
+            return;
+        }
+        if (selectedRack != currentlySelectedRack) {
+            currentlySelectedRack = selectedRack;
+            lvwShelf.getItems().clear();
+            lvwPosition.getItems().clear();
+            if (selectedRack != null) {
+                lvwShelf.getItems().setAll(currentlySelectedRack.getShelves());
+            }
+            return;
+        }
+        if (selectedShelf != currentlySelectedShelf) {
+            lvwPosition.getItems().removeAll();
+            if (selectedShelf != null) {
+                currentlySelectedShelf = selectedShelf;
+                lvwPosition.getItems().setAll(currentlySelectedShelf.getPositions());
+            }
+        }
+    }
+    private boolean canParseToDouble(TextField txf) {
+        boolean cannotParse = false;
+        try {
+            double returnValue = Double.parseDouble(txf.getText().trim());
+        } catch (NumberFormatException exception) {
+            cannotParse = true;
+            txf.setStyle("-fx-border-color: red;");
+            txf.setOnMouseClicked(e -> {txf.setStyle("-fx-border-color: transparent");});
+        }
+        return cannotParse;
+    }
+
+    private void updateWarehouses() {
+        lvwWarehouse.getItems().setAll(MainController.getStorage().getWarehouses());
+    }
 }
+
