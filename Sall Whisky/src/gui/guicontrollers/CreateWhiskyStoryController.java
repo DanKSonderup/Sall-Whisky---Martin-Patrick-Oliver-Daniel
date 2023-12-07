e5b57588b958e54483ba9192f88450500f34dd18
PatrickTyme <Patricktyme@hotmail.com>
2023-12-07 10:32:50 +0100

    Whiskystory med semi funktionalitet

--- Sall Whisky/src/gui/guicontrollers/CreateWhiskyStoryController.java
+++
@@ -1,4 +0,0 @@
-package gui.guicontrollers;
-
-public class CreateWhiskyStoryController {
-}
---
+++ Sall Whisky/src/gui/guicontrollers/CreateWhiskyStoryViewController.java
@@ -0,0 +1,41 @@
+package gui.guicontrollers;
+
+import controller.Controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateWhiskyStoryViewController implements Initializable {
+
+        @FXML
+        private Button btnCopyStory;
+
+        @FXML
+        private Label whiskyStorylbl;
+
+        @FXML
+        private TextArea whiskyStorytxa;
+
+        @FXML
+        void btnCopyStory(ActionEvent event) {
+            String ctc = whiskyStorytxa.getText().toString();
+            StringSelection stringSelection = new StringSelection(ctc);
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clipboard.setContents(stringSelection, null);
+        }
+
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+//            whiskyStorytxa.setText(Controller.generateStoryForWhisky());
+        }
+
+    }
+
