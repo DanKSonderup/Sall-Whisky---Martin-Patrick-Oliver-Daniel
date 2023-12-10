f860407f9659ac50127adf52045f767aa376a0a4
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2023-12-10 13:22:41 +0100

    Small updates to CreateWhiskyView og CreateCask

--- Sall Whisky/src/gui/guicontrollers/CreateCaskViewController.java
+++ Sall Whisky/src/gui/guicontrollers/CreateCaskViewController.java
@@ -4,11 +4,18 @@ import controller.Controller;
 import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import model.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
